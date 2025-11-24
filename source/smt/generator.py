faad82e986c71f4aa82b4aaddcbc9d3b41cb0989
vale-p-i <valeria.piscitelli@outlook.it>
2025-11-24 17:19:40 +0100

    Added main to execute all instances

--- source/smt/generator.py
+++ source/smt/generator.py
@@ -456,6 +456,120 @@ def generate_optimal_model(n, filename="schedule.smt2"):
         f.close()
 
 
+def generate_optimal_model2(n, filename="schedule.smt2"):
+
+    # Check if the number of teams is even
+    if n % 2 != 0:
+        raise ValueError("n must be even!")
+
+    weeks = n - 1
+    periods = n // 2
+    team_matches = dict((i, list()) for i in range(1, n + 1))
+    # a dictionary where the keys are the teams and the values are the matches of that team
+
+    schedule = kirkman_tournament(n)
+
+    with open(filename, "w") as f:
+        f.write("(set-option :produce-models true)\n")
+        f.write("(set-option :opt.priority box)\n")
+        f.write("(set-logic QF_UFLIA)\n")  # Set the theory
+
+        # Variables Definition
+        for week in schedule:
+            for (t1, t2) in week:
+                team_matches[t1].append((t1, t2))
+                team_matches[t2].append((t1, t2))
+                for i in range(1, periods + 1):
+                    f.write(f"(declare-fun m_{t1}_{t2}_p{i} () Bool)\n")
+
+        # Variables for optimization
+        for week in schedule:
+            for (t1, t2) in week:
+                f.write(f"(declare-fun h_{t1}_{t2} () Bool)\n")
+
+        f.write("(declare-const bound Int)\n")
+        f.write("(assert (>= bound 1))\n")
+        f.write(f"(assert (<= bound {n-1}))\n")
+
+        """for t in range(1, n + 1):
+            f.write(f"(declare-fun diff_{t} () Int)\n")"""
+
+        # Fix First Week (Symmetry Breaking)
+        p = 1
+        for (t1, t2) in schedule[0]:
+            for i in range(1, periods + 1):
+                if i == p:
+                    f.write(f"(assert m_{t1}_{t2}_p{i})\n")
+                else:
+                    f.write(f"(assert (not m_{t1}_{t2}_p{i}))\n")
+            p = p + 1
+
+        # Additional Constraint N. 1: Each match assigned to exactly one period
+        for week in schedule:
+            for (t1, t2) in week:
+                f.write("(assert (= 1 (+")
+                for i in range(1, periods + 1):
+                    f.write(f" (ite m_{t1}_{t2}_p{i} 1 0)")
+                f.write(")))\n")
+        f.write("\n")
+
+        # Additional Constraint N. 2: Each period per week holds exactly one match
+        for week in schedule:
+            for i in range(1, periods + 1):
+                f.write("(assert (= 1 (+")
+                for (t1, t2) in week:
+                    f.write(f" (ite m_{t1}_{t2}_p{i} 1 0)")
+                f.write(")))\n")
+        f.write("\n")
+
+        # Constraint N. 3: Every team plays at most twice in the same period
+        for team, matches in team_matches.items():
+            for i in range(1, periods + 1):
+                f.write("(assert (<= (+")
+                for (t1, t2) in matches:
+                    f.write(f" (ite m_{t1}_{t2}_p{i} 1 0)")
+                f.write(") 2))\n")
+
+        f.write("\n")
+
+        """for t in range(1, n + 1):
+            f.write(f"(assert (and (>= diff_{t} 1) (<= diff_{t} {n - 1})))")"""
+
+        for team, matches in team_matches.items():
+            sum1 = ""
+            sum2 = ""
+            for (t1, t2) in matches:
+                if team == t1:
+                    sum1 = sum1 + f" (ite h_{t1}_{t2} 1 0)"  # home games
+                    sum2 = sum2 + f" (ite h_{t1}_{t2} 0 1)"  # away games
+                elif team == t2:
+                    sum1 = sum1 + f" (ite h_{t1}_{t2} 0 1)"  # home games
+                    sum2 = sum2 + f" (ite h_{t1}_{t2} 1 0)"  # away games
+
+            assertion1 = f"(assert (>= bound (- (+" + sum1 + ") (+" + sum2 + "))))\n"
+            assertion2 = f"(assert (>= bound (- (- (+" + sum1 + ") (+" + sum2 + ")))))\n"
+            f.write(assertion1)
+            f.write(assertion2)
+
+            # assertion = f"(assert (= diff_{team} (abs (- (+" + sum1 + ") (+" + sum2 + ")))))\n"
+            # f.write(assertion)
+
+        f.write("\n")
+
+        f.write("(minimize bound)\n")
+        """for t in range(1, n + 1):
+            f.write(f" diff_{t}")
+        f.write("))\n")"""
+
+        f.write("(check-sat)\n")
+        for week in schedule:
+            for (t1, t2) in week:
+                for p in range(1, periods + 1):
+                    f.write(f"(get-value (m_{t1}_{t2}_p{p}))\n")
+        f.write("(get-objectives)\n")
+        f.close()
+
+
 # too much time
 def model(n, solver_name: str):
     # Check if the number of teams is even
@@ -580,30 +694,40 @@ def solution_to_matrix(n: int, solution: list):
     for _ in range(n // 2):
         schedule.append([])
 
-    for s in solution:  # getting the matches from the solution
-        s = s.split(" ")
-        if s[1][0] == "t":
-            match = s[0].split("_")
+    x = (n - 1) * (n // 2) * (n // 2)
+    matches = solution[:x]  # taking the matches variables from the solution
+    obj = solution[x:]
+    for m in matches:  # getting the matches from the solution
+        m = m.split(" ")
+        if m[1][0] == "t":
+            match = m[0].split("_")
             period = int(match[-1][1:])
             schedule[period - 1].append([int(match[1]), int(match[2])])
-    return schedule
+    if len(solution) > x:
+        obj = obj[-2].split(" ")[-1][:-1]  # objective function value
+    return schedule, obj
 
 
-def solution_to_matrix_converter_optmathsat(n: int, solution: list):
+def solution_to_matrix_optimathsat(n: int, solution: list):
     schedule = []  # list for the periods
     for _ in range(n // 2):
         schedule.append([])
 
-    for s in solution:  # getting the matches from the solution
-        s = s.split(" ")
-        if s[2][0] == "t":
-            match = s[1].split("_")
+    x = (n - 1) * (n // 2) * (n // 2)
+    matches = solution[:x]  # taking the matches variables from the solution
+    obj = solution[x:]
+    for m in matches:  # getting the matches from the solution
+        m = m.split(" ")
+        if m[2][0] == "t":
+            match = m[1].split("_")
             period = int(match[-1][1:])
             schedule[period - 1].append([int(match[1]), int(match[2])])
-    return schedule
+    if len(solution) > x:
+        obj = obj[-2].split(" ")[-1][:-1]  # objective function value
+    return schedule, obj
 
 
-def generate_json(n: int, result: dict):
+def generate_json(n: int, result: dict, solver:str):
     # check if there exists a file .json for n
     try:
         with open(f"../../res/SMT/{n}.json", mode="r", encoding="utf-8") as f:
@@ -614,73 +738,147 @@ def generate_json(n: int, result: dict):
         previous_results = dict()
 
     # put the result inside the dict and put it into the .json file
-    previous_results[solver_type] = result
+    previous_results[solver] = result
     with open(f"../../res/SMT/{n}.json", mode="w", encoding="utf-8") as f:
-        json.dump(previous_results, f)
+        json.dump(previous_results, f, indent=1)
 
 
-solver_type = "yices-smt2"
-n = 6
+def solve(n: int, solver_type: str, mode: str):
 
-s = time.time()
-generate_decisional_model(n)
-try:
-    result = subprocess.run(
-        [solver_type, "schedule.smt2"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        timeout=300
-    )
-    solution = result.stdout
-    e = time.time()
-    runtime = math.floor(e - s)
-    # splitting the file
-    solution = solution.split("\n")
-    # checking if the solution is sat
-    if solution[0] == "sat":
-        # schedule = solution_to_matrix_converter_optmathsat(n, solution[1:])
-        schedule = solution_to_matrix(n, solution[1:-1])  # removing "sat" and last line
+    if mode == "decisional":
+        s = time.time()
+        generate_decisional_model(n)
+        try:
+            result = subprocess.run(
+                [solver_type, "schedule.smt2"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                timeout=300
+            )
+            solution = result.stdout
+            e = time.time()
+            runtime = math.floor(e - s)
+            # splitting the file
+            solution = solution.split("\n")
+            # checking if the solution is sat
+            if solution[0] == "sat":
+                if solver_type == "optimathsat":
+                    schedule, obj = solution_to_matrix_optimathsat(n, solution[1:-1])  # removing "sat" and last line
+                else:
+                    schedule, obj = solution_to_matrix(n, solution[1:-1])  # removing "sat" and last line
 
-        for s in schedule:
-            print(s)
+                for s in schedule:
+                    print(s)
 
-        result = {
-            "time": runtime,
-            "optimal": False,
-            "obj": None,
-            "sol": schedule
-        }
+                result = {
+                    "time": runtime,
+                    "optimal": False,
+                    "obj": None,
+                    "sol": schedule
+                }
 
-        generate_json(n, result)
+                generate_json(n, result, solver_type)
 
-    elif solution[0] == "unsat":
+            elif solution[0] == "unsat":
 
-        result = {
-            "time": runtime,
-            "optimal": True,
-            "obj": None,
-            "sol": []
-        }
+                result = {
+                    "time": runtime,
+                    "optimal": True,
+                    "obj": None,
+                    "sol": []
+                }
 
-        generate_json(n, result)
+                generate_json(n, result, solver_type)
 
-except subprocess.TimeoutExpired:
-    solution = None
-    runtime = 300
+        except subprocess.TimeoutExpired:
+            runtime = 300
 
-    result = {
-        "time": runtime,
-        "optimal": False,
-        "obj": None,
-        "sol": []
-    }
+            result = {
+                "time": runtime,
+                "optimal": False,
+                "obj": None,
+                "sol": []
+            }
 
-    generate_json(n, result)
+            generate_json(n, result, solver_type)
+
+    elif mode == "optimal":
+        s = time.time()
+        generate_optimal_model(n)
+        try:
+            if solver_type == "z3":
+                result = subprocess.run(
+                    [solver_type, "schedule.smt2"],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                    timeout=300
+                )
+
+                solution = result.stdout
+                e = time.time()
+                runtime = math.floor(e - s)
+                print(runtime)
+
+                solution = solution.split("\n")
+                # checking if the solution is sat
+                if solution[0] == "sat":
+                    schedule, obj = solution_to_matrix(n, solution[1:-1])  # removing "sat" and last line
+
+                    for s in schedule:
+                        print(s)
+
+                    result = {
+                        "time": runtime,
+                        "optimal": True,
+                        "obj": obj,
+                        "sol": schedule
+                    }
+
+                    generate_json(n, result, solver_type + "_opt")
+            else:
+                result = subprocess.run(
+                    [solver_type, "-optimization=true", "schedule.smt2"],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                    timeout=300
+                )
+
+                solution = result.stdout
+                e = time.time()
+                runtime = math.floor(e - s)
+                print(runtime)
+
+                solution = solution.split("\n")
+
+                # checking if the solution is sat
+                if solution[0] == "sat":
+                    schedule, obj = solution_to_matrix_optimathsat(n, solution[1:-1])  # removing "sat" and last line
+
+                    for s in schedule:
+                        print(s)
+
+                    result = {
+                        "time": runtime,
+                        "optimal": True,
+                        "obj": obj,
+                        "sol": schedule
+                    }
+
+                    generate_json(n, result, solver_type + "_opt")
+
+        except subprocess.TimeoutExpired:
+            runtime = 300
+            print(runtime)
 
 
+def main():
+    for n in range(6, 21, 2):
+        print(f"n = {n}")
+        solve(n, "z3", "optimal")
 
 
-
-
-
+if __name__ == "__main__":
+    main()
