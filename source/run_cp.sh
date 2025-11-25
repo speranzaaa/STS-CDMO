2b47cd861863b99753d4e28ca9f2a291329f0efe
Lingrui <110005485+lingrrz@users.noreply.github.com>
2025-11-25 23:17:02 +0800

    Enhance run_cp.sh
    Added error handling for Docker build failure and passed arguments to the Python script.

--- source/run_cp.sh
+++ source/run_cp.sh
@@ -5,17 +5,22 @@ IMAGE_NAME="sts_solver_group"
 echo "Building Docker image..."
 docker build -q -t $IMAGE_NAME . > /dev/null
 
+if [ $? -ne 0 ]; then
+    echo "Build failed."
+    exit 1
+fi
+
 HOST_DIR=$(pwd -W)
 echo "Mounting: $HOST_DIR"
 
 echo "--------------------------------"
-echo "Running CP experiments with UNLIMITED resources..."
+echo "Running CP experiments ..."
 
 docker run --rm \
     --ulimit stack=-1 \
     --memory-swap=-1 \
     -v "$HOST_DIR://sts_solver" \
     $IMAGE_NAME \
-    python3 source/CP/run_cp.py
+    python3 source/CP/run_cp.py "$@"
 
 echo "Done."
